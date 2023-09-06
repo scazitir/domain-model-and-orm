2ff68449c2020f419e0f682049bcc69fbed00d89
Raphael Scaziti <raphascaziti@gmail.com>
2023-09-06 11:22:51 -0600

    Created entities for Order and User

---
+++ src/main/java/com/scaziti/dscommerce/entities/Order.java
@@ -0,0 +1,63 @@
+package com.scaziti.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne // Posso ter muitas ordens, para um unico cliente
+    @JoinColumn(name = "client_id") //defino o nome da foreign key
+    private User client;
+
+    public Order(){}
+
+    public Order(Long id, Instant moment, OrderStatus status, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+}
---
+++ src/main/java/com/scaziti/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.scaziti.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
