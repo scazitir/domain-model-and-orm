0b9649d6ad2932e21d4fc007ba87c8751414d733
Raphael Scaziti <raphascaziti@gmail.com>
2023-09-11 14:54:00 -0600

    refactor: Included a Set of OrderItem, getItems, and getProducts

--- src/main/java/com/scaziti/dscommerce/entities/Order.java
+++ src/main/java/com/scaziti/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.scaziti.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -22,6 +25,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){}
 
@@ -71,5 +77,13 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
