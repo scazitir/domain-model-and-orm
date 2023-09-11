107eaae6cd27ad255c31ac35acbcea4f2c9b22e3
Raphael Scaziti <raphascaziti@gmail.com>
2023-09-11 15:38:37 -0600

    refactor: Included 2 new users to the database seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -57,6 +57,8 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
 
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '123456', '2001-07-25');
 INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '123456', '1987-12-13');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Batman', 'batman@gmail.com', '977777776', '123456', '2013-12-13');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Cookie', 'cookie@gmail.com', '977777758', '123456', '2015-12-13');
 
 INSERT INTO tb_order (moment, status, client_id) VALUES (TIMESTAMP WITH TIME ZONE '2022-07-25T13:00:00Z', 1, 1);
 INSERT INTO tb_order (moment, status, client_id) VALUES (TIMESTAMP WITH TIME ZONE '2022-07-29T15:50:00Z', 3, 2);
